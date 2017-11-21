96a84fc3cefef4e0ff855f9f6c035db27ea6211f
Max Stubbe <mcj.stubbe@student.avans.nl>
2017-11-21 10:51:41 +0100

    initial

--- model/user.model.js
+++ model/user.model.js
@@ -1,8 +1,17 @@
 const mongoose = require('mongoose');
+const connection = require('../config/mongo.db')
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-    name: String
+    name: String,
+    recipes: [{
+        name: String,
+        ingredients: [{
+            name: String,
+            weight: Number,
+            price: String
+        }]
+    }]
 }, {
     timestamps: true
 });
@@ -10,9 +19,22 @@ const UserSchema = new Schema({
 
 const User = mongoose.model('user', UserSchema);
 
+//beforeEach(() => {
+//   connection.collections.user.drop();
+//});
+
 // Add a 'dummy' user (every time you require this file!)
 const user = new User({
     name: 'Joe',
-}).save();
+    recipes:[
+        { name: "Pizza", 
+        ingredients: [{
+            name: "kaas", weight: 42069, price: "3,80"
+        }]},
+        { name:"Cake", 
+        ingredients: [{
+            name: "eieren", weight: 2, price: "4,00"
+        }]}
+    ]}).save();
 
 module.exports = User;
