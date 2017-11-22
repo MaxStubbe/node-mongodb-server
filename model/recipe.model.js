6554b93716e4862266ab96dd893db29d7163a7b8
Max Stubbe <mcj.stubbe@student.avans.nl>
2017-11-22 19:12:15 +0100

    db vullen

--- model/recipe.model.js
+++ model/recipe.model.js
@@ -14,4 +14,24 @@ const RecipeSchema = new Schema({
 
 const Recipe = mongoose.model('recipe', RecipeSchema);
 
+const recipe1 = new Recipe({
+    name: 'Tasty Schnitzel',
+    discription: 'A super-tasty Schnitzel - just awesome!',
+    imagePath: 'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
+    ingredients : [
+        {name: "Meat", amount: 1 },
+        { name: "French Fries", amount: 20 }
+    ]
+}).save();
+
+const recipe2 =  new Recipe({
+    name: 'Big Fat Burger',
+    discription: 'What else you need to say?',
+    imagePath: 'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
+    ingredients : [
+        {name: "Buns", amount: 2 },
+        { name: "Meat", amount: 1 }
+    ]
+}).save();
+
 module.exports = Recipe;
